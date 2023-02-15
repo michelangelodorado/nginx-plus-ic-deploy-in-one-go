7cbf2a21ac50a10951f8be795a0d795050e465e6
ericausente <eric_ausente@yahoo.com>
2023-02-15 23:30:08 +0800

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-kubectl delete secret regcred 
 kubectl delete -f 1-RBAC/
-kubectl delete -f 2-CommonResources/
+kubectl delete -f 2-CommonResources/ingress-class.yaml
 kubectl delete -f 3-CustomResources/
-kubectl delete -f 4-nginx-deploy/
-kubectl delete -f 5-service/
-
