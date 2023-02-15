86db2ca114e0602abef9a8237a4e56851dd2e06f
ericausente <eric_ausente@yahoo.com>
2023-02-15 23:33:07 +0800

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+kubectl delete -f https://raw.githubusercontent.com/nginxinc/kubernetes-ingress/main/examples/custom-resources/basic-configuration/cafe.yaml
+kubectl delete -f https://raw.githubusercontent.com/nginxinc/kubernetes-ingress/main/examples/custom-resources/basic-configuration/cafe-secret.yaml
+kubectl delete -f https://raw.githubusercontent.com/nginxinc/kubernetes-ingress/main/examples/custom-resources/basic-configuration/cafe-virtual-server.yaml
+
 kubectl delete -f 1-RBAC/
 kubectl delete -f 2-CommonResources/ingress-class.yaml
 kubectl delete -f 3-CustomResources/
+
